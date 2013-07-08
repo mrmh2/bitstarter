6e2258d27020c0fd4ce68eb29fc4c4b9339f4f52
Matthew Hartley <mhartley@cantab.net>
2013-07-08 13:15:11 +0100

    Now reading from index.html and returning as response.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buf');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	index_data = fs.readFileSync('index.html');
+	index_string = buf.toString(index_data);
+	response.send(index_string);
 });
 
 var port = process.env.PORT || 5000;
