5d9758da6e6c328a30baa3bb48af440bc8c5a13b
Matthew Hartley <mhartley@cantab.net>
2013-07-10 11:53:31 +0100

    Added proper URL handling and full checks file

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -55,20 +56,62 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkString = function(string, checksfile) {
+    $ = cheerio.load(string);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var response2console = function(result, response) {
+    if (result instanceof Error) {
+        console.error('Error: ' + util.format(response.message));
+    } else {
+        console.log(result);
+    }
+};
+
+var buildHandler = function(checks) {
+
+    var printOutput = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            var checkJson = checkString(result, checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    }
+
+    return printOutput;
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    // This is a bit ugly, if we have a URL we use it, if not we end up with the file default
+    if (typeof program.url === "undefined") {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } else {
+        //console.log('Reading from: ' + program.url);
+        var urlHandler = buildHandler(program.checks);
+        rest.get(program.url).on('complete', urlHandler);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 };
